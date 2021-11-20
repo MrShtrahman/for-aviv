9c277c946a4ec7c7ea36e3d8ff744ffd9e75f986
MrShtrahman <matashual@gmail.com>
2021-11-20 10:18:29 +0200

    Changing to vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
+import './index.css'
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { CookiesProvider } from 'react-cookie';
-import store from './redux/store';
+import store from 'redux/store';
 import 'semantic-ui-css/semantic.min.css'
-import './index.css';
 import App from './App'
 
 render(
   <StrictMode>
-    <CookiesProvider>
-      <Provider {...{ store }}>
-        <App />
-      </Provider>
-    </CookiesProvider>
+    <Provider {... { store }}>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById('root')
-);
+)
