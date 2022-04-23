20a53c6fe8cf3b8455ce88dc5ea326d919cc1818
MrShtrahman <matashual@gmail.com>
2022-04-23 11:47:04 +0300

    Degrading react back to 17 (because of semantic-ui-react)

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import './index.css';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 
-const rootContainer = document.getElementById('root');
-const root = createRoot(rootContainer!);
-
-root.render(
+render(
   <StrictMode>
     <Provider {...{ store }}>
       <App />
     </Provider>
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById('root')
 );
