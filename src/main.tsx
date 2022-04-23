4b1544d963a06fca8921ebce6cb404adc75baa60
MrShtrahman <matashual@gmail.com>
2022-04-23 10:47:41 +0300

    Upgrade to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import './index.css';
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 
-render(
+const rootContainer = document.getElementById('root');
+const root = createRoot(rootContainer!);
+
+root.render(
   <StrictMode>
     <Provider {...{ store }}>
       <App />
     </Provider>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
