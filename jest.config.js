bafcb464c911d6790e936b962883a7772a9963e2
MrShtrahman <matashual@gmail.com>
2022-12-30 18:53:53 +0200

    Whoops

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    moduleNameMapper: {
-      "\\.(css|sass|scss)$": "identity-obj-proxy",
-    },
-    collectCoverage: true,
-    moduleDirectories: ['node_modules', 'src']
-  };
